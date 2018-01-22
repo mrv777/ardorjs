0e312739b420476c4f9f6c072b84930401aa12a8
Leon <toxiapo@gmail.com>
2018-01-22 16:33:20 -0500

    add files

---
+++ README.md
@@ -0,0 +1,8 @@
+# ardorjs
+
+Transaction signing and address creation for ARDOR
+
+Original author: [toenu23](https://github.com/toenu23/nxtjs),  modified for Ardor platform
+
+### npm install
+`npm install ardorjs`
---
+++ index.js
@@ -0,0 +1,13 @@
+(function() {
+
+  var account = require('./lib/account');
+
+  module.exports = {
+    rsConvert: account.rsConvert,
+    secretPhraseToPublicKey: account.secretPhraseToPublicKey,
+    publicKeyToAccountId: account.publicKeyToAccountId,
+    secretPhraseToAccountId: account.secretPhraseToAccountId,
+    signTransactionBytes: account.signTransactionBytes
+  };
+
+})();
