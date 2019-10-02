5589a5ab3dd5b7e1fd873e8237a3e916741a938e
Atzen2 <A_Aldemir@hotmail.de>
2019-10-02 16:59:58 +0200

    changed test framework to jest

---
+++ jest.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+    "roots": [
+      "<rootDir>/test"
+    ]
+    // "testEnvironment": "node"
+  }
--- test/lib/account.test.js
+++ test/lib/account.test.js
@@ -1,6 +1,3 @@
-var assert = require('assert');
-var expect = require('chai').expect;
-var should = require('chai').should();
 
 import {
   secretPhraseToPublicKey,
